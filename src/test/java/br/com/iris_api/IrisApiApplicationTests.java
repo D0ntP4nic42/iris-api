e56217fc1a6a9c05c058c777fb6f8624fbe29d83
Leonardo <leozinngomes@gmail.com>
2025-01-16 10:35:35 -0300

    initial commit

---
+++ src/test/java/br/com/iris_api/IrisApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.iris_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IrisApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
