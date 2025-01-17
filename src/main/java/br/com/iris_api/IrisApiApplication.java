cd0e7bb0ba948f99a448b03ec5dcb59453d259ff
Leonardo <leozinngomes@gmail.com>
2025-01-17 11:47:52 -0300

    Fix temporário para o problema de bean dos repositories

--- src/main/java/br/com/iris_api/IrisApiApplication.java
+++ src/main/java/br/com/iris_api/IrisApiApplication.java
@@ -2,8 +2,10 @@ package br.com.iris_api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories("br.com.iris_api.repository")
 public class IrisApiApplication {
 
 	public static void main(String[] args) {
