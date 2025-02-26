200e68902d012426f8f6c98a0438f33400d5a497
D0ntP4nic <leozinngomes@gmail.com>
2025-02-26 20:17:31 -0300

    Removendo annotation duplicada de JPA

--- src/main/java/br/com/iris_api/IrisApiApplication.java
+++ src/main/java/br/com/iris_api/IrisApiApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EnableJpaRepositories("br.com.iris_api.repository")
 public class IrisApiApplication {
 
 	public static void main(String[] args) {
