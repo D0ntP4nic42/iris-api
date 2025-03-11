fabf9b28ad53dedf295c1462ce175298ff82c422
Leonardo <leozinngomes@gmail.com>
2025-03-11 12:54:41 -0300

    Correção @Repository no UserRepository

--- src/main/java/br/com/iris_api/repository/UserRepository.java
+++ src/main/java/br/com/iris_api/repository/UserRepository.java
@@ -3,9 +3,11 @@ package br.com.iris_api.repository;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import br.com.iris_api.entity.User;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 	public Optional<User> findByCpf(String cpf);
 }
