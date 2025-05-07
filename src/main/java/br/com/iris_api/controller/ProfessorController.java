781a45eda70fa5e4413e8b77d66530b5206a7d32
D0ntP4nic <leozinngomes@gmail.com>
2025-05-07 17:55:23 -0300

    Documentando endpoints de Professor

--- src/main/java/br/com/iris_api/controller/ProfessorController.java
+++ src/main/java/br/com/iris_api/controller/ProfessorController.java
@@ -1,12 +1,12 @@
 package br.com.iris_api.controller;
 
 import java.security.Principal;
-import java.util.Collections;
 import java.util.Map;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -15,9 +15,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.iris_api.dto.ProfessorRegisterDTO;
+import br.com.iris_api.entity.Professor;
 import br.com.iris_api.service.ProfessorService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
-import jakarta.persistence.EntityNotFoundException;
 
 @RestController
 @RequestMapping("/professores")
@@ -28,79 +32,62 @@ public class ProfessorController {
 	@Autowired
 	private ProfessorService professorService;
 
+	@Operation(summary = "Listar professores", description = "Retorna a lista de professores cadastrados com alguns limites de dados")
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "200", description = "Professores listados com sucesso", content = @Content()),
+			@ApiResponse(responseCode = "500", description = "Um erro desconhecido ocorreu", content = @Content()) })
 	@GetMapping
-	public ResponseEntity listarProfessores() {
-		try {
-			var response = professorService.listar().stream().map(professor -> Map.of("nome", professor.getNome(),
-					"role", professor.getRole(), "id", professor.getId())).collect(Collectors.toList());
+	public ResponseEntity<?> listarProfessores() {
+		var response = professorService.listar().stream().map(
+				professor -> Map.of("nome", professor.getNome(), "role", professor.getRole(), "id", professor.getId()))
+				.collect(Collectors.toList());
 
-			return ResponseEntity.ok().body(response);
-		} catch (Exception e) {
-			e.printStackTrace();
-			return ResponseEntity.badRequest()
-					.body(Collections.singletonMap(RESPONSE_FIELD_NAME, "Erro ao listar professores"));
-		}
+		return ResponseEntity.ok().body(response);
 	}
 
+	@Operation(summary = "Buscar informações do professor logado", description = "Retorna as informações de um professor")
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "200", description = "Professor encontrado com sucesso", content = @Content()),
+			@ApiResponse(responseCode = "404", description = "Professor não encontrado", content = @Content()),
+			@ApiResponse(responseCode = "500", description = "Um erro desconhecido ocorreu", content = @Content()) })
 	@GetMapping("/info-conta")
-	public ResponseEntity infoConta(Principal principal) {
-		try {
-			var professor = professorService.findByUsername(principal.getName());
+	public ResponseEntity<Professor> infoConta(Principal principal) {
+		var professor = professorService.findByUsername(principal.getName());
 
-			if (professor.isEmpty()) {
-				return ResponseEntity.badRequest()
-						.body(Collections.singletonMap(RESPONSE_FIELD_NAME, "Professor não encontrado"));
-			}
-			return ResponseEntity.ok().body(professor.get());
-		} catch (Exception e) {
-			e.printStackTrace();
-			return ResponseEntity.badRequest()
-					.body(Collections.singletonMap(RESPONSE_FIELD_NAME, "Erro ao buscar professorCPF"));
-		}
+		return ResponseEntity.ok().body(professor);
 	}
 
+	@Operation(summary = "Alterar informações do professor logado", description = "Altera as informações de um professor")
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "200", description = "Professor alterado com sucesso", content = @Content()),
+			@ApiResponse(responseCode = "401", description = "Usuário não possui autorização", content = @Content()),
+			@ApiResponse(responseCode = "500", description = "Um erro desconhecido ocorreu", content = @Content()) })
 	@PutMapping("/alterar-conta")
-	public ResponseEntity alterarConta(@RequestBody ProfessorRegisterDTO professorDTO, Principal principal) {
-		try {
-			var professorLogado = professorService.findByUsername(principal.getName())
-					.orElseThrow(() -> new EntityNotFoundException("Usuário não possui autorização"));
+	public ResponseEntity<String> alterarConta(@RequestBody ProfessorRegisterDTO professorDTO, Principal principal) {
+		var professorLogado = professorService.findByUsername(principal.getName());
 
-			if (!professorLogado.getCpf().equals(professorDTO.cpf())) {
-				return ResponseEntity.badRequest()
-						.body(Collections.singletonMap(RESPONSE_FIELD_NAME, "Usuário não possui autorização"));
-			}
-
-			var professor = professorService.alterar(professorDTO);
-			
-			if (professor == null) {
-				return ResponseEntity.badRequest()
-						.body(Collections.singletonMap(RESPONSE_FIELD_NAME, "Erro ao alterar professor"));
-			}
-
-			return ResponseEntity.ok()
-					.body(Collections.singletonMap(RESPONSE_FIELD_NAME, "Conta alterado com sucesso"));
-
-		} catch (Exception e) {
-			e.printStackTrace();
-			return ResponseEntity.badRequest()
-					.body(Collections.singletonMap(RESPONSE_FIELD_NAME, "Erro ao alterar professor"));
+		if (!professorLogado.getCpf().equals(professorDTO.cpf())) {
+			throw new BadCredentialsException("Você não pode alterar informações de outro professor");
 		}
+
+		var professor = professorService.alterar(professorDTO);
+
+		if (professor == null) {
+			throw new RuntimeException("Erro ao alterar professor");
+		}
+
+		return ResponseEntity.ok().body("Conta alterado com sucesso");
+
 	}
 
+	@Operation(summary = "Deletar conta do professor logado", description = "Remove a conta do professor logado")
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "200", description = "Conta removida com sucesso", content = @Content()),
+			@ApiResponse(responseCode = "404", description = "Professor não encontrado", content = @Content()),
+			@ApiResponse(responseCode = "500", description = "Um erro desconhecido ocorreu", content = @Content()) })
 	@DeleteMapping("/deletar-conta")
-	public ResponseEntity deletarConta(Principal principal) {
-		try {
-			if (professorService.findByUsername(principal.getName()).isEmpty()) {
-				return ResponseEntity.badRequest()
-						.body(Collections.singletonMap(RESPONSE_FIELD_NAME, "Professor não encontrado"));
-			}
-			professorService.deletar(principal.getName());
-			return ResponseEntity.ok()
-					.body(Collections.singletonMap(RESPONSE_FIELD_NAME, "Conta removida com sucesso"));
-		} catch (Exception e) {
-			e.printStackTrace();
-			return ResponseEntity.badRequest()
-					.body(Collections.singletonMap(RESPONSE_FIELD_NAME, "Erro ao remover conta"));
-		}
+	public ResponseEntity<String> deletarConta(Principal principal) {
+		professorService.deletar(principal.getName());
+		return ResponseEntity.ok().body("Conta removida com sucesso");
 	}
 }
