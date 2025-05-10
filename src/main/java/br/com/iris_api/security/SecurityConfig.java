684a3e30a154af10ac7364106512f45528d1a993
D0ntP4nic <leozinngomes@gmail.com>
2025-05-09 21:19:15 -0300

    Removendo imports inúteis Security Config

--- src/main/java/br/com/iris_api/security/SecurityConfig.java
+++ src/main/java/br/com/iris_api/security/SecurityConfig.java
@@ -3,13 +3,11 @@ package br.com.iris_api.security;
 import java.security.interfaces.RSAPrivateKey;
 import java.security.interfaces.RSAPublicKey;
 import java.util.List;
-import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
