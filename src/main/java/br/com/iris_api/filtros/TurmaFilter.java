6d298aa14d8351a30b8d0452d09e72e0b774dbf6
D0ntP4nic <leozinngomes@gmail.com>
2025-05-20 19:51:21 -0300

    fix filter de turma

--- src/main/java/br/com/iris_api/filtros/TurmaFilter.java
+++ src/main/java/br/com/iris_api/filtros/TurmaFilter.java
@@ -1,5 +1,6 @@
 package br.com.iris_api.filtros;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -12,30 +13,28 @@ import br.com.iris_api.entity.Turma;
 public record TurmaFilter(String identificador, String sala, Map<String, String> disciplinas, List<String> nomesAlunos,
 		String temporadaLetiva) {
 	public static TurmaFilter fromEntity(Turma turma) {
-		Map<String, String> disciplinasMap = turma.getDisciplinas().stream()
-				.collect(Collectors.toMap(d -> d.getProfessor().getNome(), Disciplina::getNome));
+		Map<String, String> disciplinasMap = new HashMap<>();
+		for (Disciplina disciplina : turma.getDisciplinas()) {
+			disciplinasMap.put(disciplina.getNome(), disciplina.getProfessor().getNome());
+		}
 
 		List<String> nomesAlunos = turma.getAlunos().stream().map(Aluno::getNome).toList();
 
 		return new TurmaFilter(turma.getIdentificador(), turma.getSala(), disciplinasMap, nomesAlunos,
 				turma.getTemporadaLetiva());
 	}
-	
+
 	public static TurmaFilter fromEntity(Turma turma, Professor professor) {
-	    Map<String, String> disciplinasMap = turma.getDisciplinas().stream()
-	        .filter(d -> d.getProfessor().getId().equals(professor.getId()))
-	        .collect(Collectors.toMap(d -> d.getProfessor().getNome(), Disciplina::getNome));
+		Map<String, String> disciplinasMap = new HashMap<>();
+		for (Disciplina disciplina : turma.getDisciplinas()) {
+			if (disciplina.getProfessor().equals(professor)) {
+				disciplinasMap.put(disciplina.getNome(), disciplina.getProfessor().getNome());
+			}
+		}
 
-	    List<String> nomesAlunos = turma.getAlunos().stream()
-	        .map(Aluno::getNome)
-	        .toList();
+		List<String> nomesAlunos = turma.getAlunos().stream().map(Aluno::getNome).toList();
 
-	    return new TurmaFilter(
-	        turma.getIdentificador(),
-	        turma.getSala(),
-	        disciplinasMap,
-	        nomesAlunos,
-	        turma.getTemporadaLetiva()
-	    );
+		return new TurmaFilter(turma.getIdentificador(), turma.getSala(), disciplinasMap, nomesAlunos,
+				turma.getTemporadaLetiva());
 	}
 }
